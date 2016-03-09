91c826e43a484fe70893de780c8b70c5ab87371d
giovanni.morelli <giovanni.morelli@accenture.com>
2016-03-09 10:51:15 +0100

    Remove Api_Key

--- public/javascripts/common.js
+++ public/javascripts/common.js
@@ -7,7 +7,7 @@ flickr.controller('setCommonVariables',function($scope){
         $scope.currentPage=0;
         $scope.lastImages =[];
         $scope.flickrApi = new Flickr({
-            api_key: "911f2fe2fe7f739d8fb262fae527e3a9"//add your api_key
+            api_key: ""//add your api_key
         });
     };
     $scope.init();
