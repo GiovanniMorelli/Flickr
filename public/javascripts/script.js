8030a5499b6310eeed541b13f3d044af0c6b288d
giovanni.morelli <giovanni.morelli@accenture.com>
2016-03-09 14:15:50 +0100

    Added  scroll top

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,31 +1,21 @@
-$( document ).ready(function() {
+$(document).on( 'scroll', function(){
 
-
-    checkForChanges();
-    $('.scroll-top-wrapper').on('click', scrollToTop);
-
+    if ($(window).scrollTop() > 100) {
+        $('.scroll-top-wrapper').addClass('show');
+    } else {
+        $('.scroll-top-wrapper').removeClass('show');
+    }
 });
 
-function checkForChanges()
-{
-    if ($('#mCSB_1_scrollbar_vertical').hasClass('mCSB_scrollTools_onDrag')){
-        if ($('#mCSB_1_dragger_vertical').css("top").replace("px","") > 100) {
-            $('.scroll-top-wrapper').addClass('show');
-        } else {
-            $('.scroll-top-wrapper').removeClass('show');
-        }
-    }
-    setTimeout(checkForChanges, 500);
 
-}
-
+$( document ).ready(function() {
+    $('.scroll-top-wrapper').on('click', scrollToTop);
+});
 
 function scrollToTop() {
     verticalOffset = typeof(verticalOffset) != 'undefined' ? verticalOffset : 0;
     element = $('body');
     offset = element.offset();
     offsetTop = offset.top;
-    $("#mCSB_1_dragger_vertical").animate({top: "0px",}, 500 );
-    $("#mCSB_1_container").animate({top: "0px",}, 500 );
-    $('.scroll-top-wrapper').removeClass('show');
+    $('html, body').animate({scrollTop: offsetTop}, 500, 'linear');
 }
